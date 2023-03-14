60438789450a8391bd1edbb553dda335058da411
Joshua <janorris65@gmail.com>
2023-03-14 01:33:04 -0500

    footer imgs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import PropTypes from "prop-types";
 
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <>
+      <a href="/">
+        <img src="../assets/Linkedinimg.jpg" alt="" />
+      </a>
+      <a href="/">
+        <img src="../assets/Instagramimg.jpg" alt="" />
+      </a>
+    </>
+  );
 }
