4ec1a645d4c0821a54fa614a352eb1cdedca69d0
Joshua <janorris65@gmail.com>
2023-03-14 01:20:44 -0500

    blur error contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,96 @@
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 
-export default function Contact() {
-  return <h1>Contact</h1>;
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from "../utils/helpers.js";
+
+function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  let blurError;
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "userName") {
+      setUserName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+  const handleBlurError = (e) => {
+    const { target } = e;
+    const inputValue = target.value;
+    if (inputValue === "") {
+      blurError = "Input Required";
+      console.log(blurError);
+      alert(blurError);
+    }
+  };
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage("Email or username is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    alert(`Hello ${userName}`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName("");
+    setMessage("");
+    setEmail("");
+  };
+
+  return (
+    <div>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          onBlur={handleBlurError}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          onBlur={handleBlurError}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default Contact;
